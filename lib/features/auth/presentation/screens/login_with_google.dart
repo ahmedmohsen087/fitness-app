8031b7e7d45b603fbd85d84b786896a139b6603a
ahmedmohsen087 <ahmedmohsen0871@gmail.com>
2026-07-11 21:59:54 +0300

    login with google

---
+++ lib/features/auth/presentation/screens/login_with_google.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../core/values/assets.dart';
+
+class LoginWithGoogle extends StatelessWidget {
+  const LoginWithGoogle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 15,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(Assets.facebookLogo),
+        SvgPicture.asset(Assets.googleLogo),
+        SvgPicture.asset(Assets.appleLogo),
+      ],
+    );
+  }
+}
